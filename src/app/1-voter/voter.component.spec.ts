519db03962170479a83eff942f0f3a2188d7b422
Christopher Miles <mail@christophermiles.co.uk>
2018-05-31 15:52:28 +0100

    Add test to check rendered text inside a component instance

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -1,10 +1,28 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { VoterComponent } from './voter.component';
 
 describe('VoterComponent', () => {
+  let component: VoterComponent;
+  let fixture: ComponentFixture<VoterComponent>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [VoterComponent]
+    });
+
+    fixture = TestBed.createComponent(VoterComponent);
+    component = fixture.componentInstance;
   });
 
-  it('', () => {
+  it('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    const debugEl = fixture.debugElement.query(By.css('.vote-count'));
+    const nativeEl: HTMLElement = debugEl.nativeElement;
+
+    expect(nativeEl.innerText).toContain('21');
   });
 });
