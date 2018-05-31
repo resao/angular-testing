e9e09fe246efe3cc0495edc4c56e798be07422df
Christopher Miles <mail@christophermiles.co.uk>
2018-05-31 15:03:34 +0100

    Adding further service spyon tests
    Add tests to ensure component calls service and our component todos instance is equal to that returned from service

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -2,6 +2,7 @@ import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -28,5 +29,26 @@ describe('TodosComponent', () => {
     component.ngOnInit();
 
     expect(component.todos).toBe(todos);
+  });
+
+  it('should call the server to save the changes when new todo is added', () => {
+    const spy = spyOn(service, 'add').and.callFake(t => {
+      return Observable.empty();
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should add the new todo returned from the server', () => {
+    const todo = { id: 1 };
+    const spy = spyOn(service, 'add').and.callFake(t => {
+      return Observable.from([todo]);
+    });
+
+    component.add();
+
+    expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
   });
 });
