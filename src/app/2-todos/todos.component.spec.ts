b18af7a2eed0855f19bc7d23ac5f7bfa94f96a91
Christopher Miles <mail@christophermiles.co.uk>
2018-05-31 17:27:27 +0100

    Example of async and fakeAsync test types

--- src/app/2-todos/todos.component.spec.ts
+++ src/app/2-todos/todos.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -34,12 +34,24 @@ describe('TodosComponent', () => {
     component = fixture.componentInstance;
   });
 
-  it('should load todos from the server', () => {
+  it('should load todos from the server (async test)', async(() => {
     const service = TestBed.get(TodoService);
-    spyOn(service, 'getTodos').and.returnValue(Observable.from([[1, 2, 3]]));
+    spyOn(service, 'getTodosPromise').and.returnValue(Promise.resolve([1, 2, 3]));
 
     fixture.detectChanges();
 
+    fixture.whenStable().then(() => {
+      expect(component.todos.length).toBe(3);
+    });
+  }));
+
+  it('should load todos from the server (fakeasync test)', fakeAsync(() => {
+    const service = TestBed.get(TodoService);
+    spyOn(service, 'getTodosPromise').and.returnValue(Promise.resolve([1, 2, 3]));
+
+    fixture.detectChanges();
+
+    tick();
     expect(component.todos.length).toBe(3);
-  });
+  }));
 });
